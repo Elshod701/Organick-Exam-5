14a135b107629aa36bd06210c18ff984e3ac98ca
Elshod701 <jorayevelshod511@gmail.com>
2024-02-19 21:16:13 +0500

    Node modules deleted

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      'roboto-regular': ['Roboto-regular'],
+      'roboto-medium': ['Roboto-medium'],
+      'roboto-bold': ['Roboto-bold'],
+      'yellowtail': ['Yellowtail']
+    },
+    extend: {
+      backgroundImage: {
+        'econis': "url('./src/assets/images/econis.png')",
+        'not-found': "url('./src/assets/images/not_found.png')",
+      }
+    },
+    colors: {
+      'primary': '#274C5B',
+      'secondary': "#7EB693",
+      'yellow': '#EFD372',
+      'gray': '#D4D4D4',
+      'transparent': 'transparent',
+      'white': 'white',
+      'section-bg': '#F9F8F8',
+      'for-text': '#525C60',
+      'cards-bg': '#F9F8F8',
+      'border': '#DEDDDD',
+      'report-bg': '#F1F1F1',
+      'product-bg': '#F1F8F4',
+    }
+  },
+  plugins: [],
+}
